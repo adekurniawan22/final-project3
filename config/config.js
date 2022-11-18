f14b77d6086f8421d62c6031e5f11782c2891219
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-11-18 13:52:48 +0700

    commit

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_USER
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "dialect": process.env.PGUSER
   },
   "test": {
     "username": "root",
