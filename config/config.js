9ad88ba70be2dba82b4eb059fb6f839f650ad598
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-11-23 18:27:54 +0700

    config deploy

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": process.env.PGUSER,
-    "password": process.env.PGPASSWORD,
-    "database": process.env.PGDATABASE,
-    "host": process.env.PGHOST,
-    "dialect": process.env.PGUSER
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
@@ -22,4 +22,5 @@ module.exports = {
     "port": process.env.PGPORT,
     "dialect": process.env.PGUSER
   }
+
 }
