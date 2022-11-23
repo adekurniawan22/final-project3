80be2b26f4496249a545de0919bfa22173c33b2d
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-11-23 18:37:56 +0700

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PGPORT || 5000;
 const userRouter = require('./routes/userRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const productRouter = require('./routes/productRoute')
