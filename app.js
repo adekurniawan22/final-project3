9933a001684aacaace669ae3797fe9c503d95f34
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-11-28 14:57:42 +0700

    tes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PGPORT || 5000;
+const PORT = process.env.PGPORT;
 const userRouter = require('./routes/userRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const productRouter = require('./routes/productRoute')
@@ -11,6 +11,9 @@ const transactionRoutes = require('./routes/transactionRoute')
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("assets"));
+app.set("view engine", "ejs");
+app.get('/', (req, res) => res.render('index'));
 app.use('/users', userRouter);
 app.use(categoryRoute);
 app.use(productRouter);
